d10e2e8a93d9fc5806382dcbbca569370906adb2
Vidit Shah <vidit0210@gmail.com>
2019-03-31 13:43:59 +0530

    recursive

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -11,6 +11,7 @@
 //     const words =[];
 //     for(let word of str.split(' ')){
 //         words.push(word[0].toUpperCase() + word.slice(1))
+
 //     }
 //     return words.join(' ')
 // }
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,8 +14,38 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    
+// function pyramid(n) {
+//     let midpoint = Math.floor((2*n-1)/2);
+//    for(let row= 0;row<n;row++){
+//        let level ='';
+//        for(let coloumn=0;coloumn<2*n-1;coloumn++){
+//             if(midpoint-row<=coloumn && midpoint+row>=coloumn){
+//                 level+="#"
+//             }else{
+//                 level+=' ';
+//             }
+//        }
+//        console.log(level)
+//    }
+// }
+
+//now With recurrsion
+function pyramid(n,row,level=''){
+    if(row===n){
+        return;
+    }
+    if(level.lenth===2*n-1){
+        console.log(level);
+        return pyramid(n,row+1)
+    }
+    const midpoint = Math.floor((2*n-1)/2)
+    let add;
+    if(midpoint-row<=level.length && midpoint + row >=level.length){
+        add="#"
+    }else{
+        add=' '
+    }
+    pyramid(n,row,level+add)
 }
 
 module.exports = pyramid;
